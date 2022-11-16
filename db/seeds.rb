bf9989f30d9a7be1b068e660bfe2f1cdb6c550e5
raphael assaf <raphassaf01@outlook.com>
2022-11-16 16:26:20 +0100

    resto et reviews

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+dishoom = {name: "Dishoom", address: "7 Boundary St, London E2 7JE", category: "japanese"}
+pizza_east =  {name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", category: "italian"}
+chicken_poulet = {name: "Chicken Poulet", address: "Heaven", category: "french"}
+si_bon_dasie = {name: "Si Bon d'Asie", address: "Parmentier", category: "chinese"}
+etoile_de_mer = {name: "L'Etoile de Mer", address: "Un peu loin", category: "chinese"}
+
+[dishoom, pizza_east, chicken_poulet, si_bon_dasie, etoile_de_mer].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
